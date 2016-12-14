bb4a708bac1970c8713e6768672864682e531048
Dmitry Lembas <lembasdima@gmail.com>
2016-12-14 23:39:45 +0200

    SimInfo application. First commit

---
+++ app/src/main/java/org/aspins/siminfo/MainActivity.java
@@ -0,0 +1,99 @@
+package org.aspins.siminfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView phoneNumberTxtView;
+    private TextView countryCodeTxtView;
+    private TextView simStateTxtView;
+    private TextView simSeriaNumberTxtView;
+    private TextView simOperatorCodeTxtView;
+    private TextView simIMSI;
+    private TextView simOperatorNetworkTxtView;
+    private TextView voiceMailTextIDTxtView;
+    private TextView voiceMailNumberTxtView;
+    private TextView phoneTypeTxtView;
+    private TextView deviceIMEITxtView;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TelephonyManager tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        String phoneNumber = tm.getLine1Number();
+        phoneNumberTxtView = (TextView)findViewById(R.id.phone_Number_Digital);
+        phoneNumberTxtView.setText(phoneNumber);
+
+        String simSerialNumber = tm.getSimSerialNumber();
+        simSeriaNumberTxtView = (TextView)findViewById(R.id.sim_Serial_Number_Digital);
+        simSeriaNumberTxtView.setText(simSerialNumber);
+
+        String countryCode = tm.getSimCountryIso();
+        countryCodeTxtView = (TextView)findViewById(R.id.sim_Country_Code_Text);
+        countryCodeTxtView.setText(countryCode);
+
+        int simState = tm.getSimState();
+
+        simStateTxtView = (TextView)findViewById(R.id.sim_State_Digital);
+        simStateTxtView.setText(String.valueOf(simState));
+
+        String simOperatorCode = tm.getSimOperator();
+        simOperatorCodeTxtView = (TextView)findViewById(R.id.sim_Operator_Code_Text);
+        simOperatorCodeTxtView.setText(simOperatorCode);
+
+        String simOperatorName = tm.getNetworkOperatorName();
+        simOperatorNetworkTxtView = (TextView)findViewById(R.id.sim_Operator_Name_Text);
+        simOperatorNetworkTxtView.setText(simOperatorName);
+
+        simIMSI = (TextView)findViewById(R.id.sim_IMSI_Digital);
+        simIMSI.setText(String.valueOf(tm.getSubscriberId()));
+
+        voiceMailTextIDTxtView = (TextView)findViewById(R.id.voice_Mail_Text_ID_Digital);
+        voiceMailTextIDTxtView.setText(tm.getVoiceMailAlphaTag());
+
+        voiceMailNumberTxtView = (TextView)findViewById(R.id.voice_Mail_Number_Digital);
+        voiceMailNumberTxtView.setText(tm.getVoiceMailNumber());
+
+        phoneTypeTxtView = (TextView)findViewById(R.id.phone_Type_Text);
+        setPhoneTypeName(tm.getPhoneType());
+        phoneTypeTxtView.setText(getPhoneTypeName());
+
+        deviceIMEITxtView = (TextView)findViewById(R.id.device_IMEI_Digital);
+        deviceIMEITxtView.setText(String.valueOf(tm.getDeviceId()));
+    }
+
+    private int pID;
+    private String pPT;
+    public void setPhoneTypeName(int id){
+        this.pID = id;
+
+        switch (pID) {
+            case 0:
+                pPT = "UNKNOWN";
+                break;
+            case 1:
+                pPT = "GSM";
+                break;
+            case 2:
+                pPT = "CDMA";
+                break;
+            case 3:
+                pPT = "SIP";
+                break;
+        }
+    }
+
+    public String getPhoneTypeName(){
+        return pPT;
+    }
+}
